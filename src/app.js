675529541a2dbb7c784cc45e23e66b4cb70b72ad
Praneetha <praneethanomula@example.com>
2025-08-30 23:26:21 +0530

    exploring get post delete

--- src/app.js
+++ src/app.js
@@ -1,18 +1,23 @@
 const express=require('express');
 const app=express();
 
-app.use("/",(req,res)=>{
-res.send("Namste node js ");
-}
-);
+
 app.use("/hello",(req,res)=>{
 res.send("hello hello hello ");
+}
+);
+app.use("/test/4",(req,res)=>{
+res.send("hello from the  ");
 }
 );
 app.use("/test",(req,res)=>{
 res.send("hello from the server ");
 }
 );
+//app.use("/",(req,res)=>{
+//res.send("Namste node js ");
+//}
+//);
 
 app.listen(7777,()=>{
     console.log("server running on port 3000");
